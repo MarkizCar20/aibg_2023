d2b7323901f78dcba5cacbdf952e044f7d2d6741
Marko Mijailovic <mijailovicmarko2000@gmail.com>
2023-12-10 06:33:53 +0100

    add state getters

--- src/api.py
+++ src/api.py
@@ -1,3 +1,25 @@
-from test import login
+from test import login, createGame, joinGame
+import asyncio
+import aiohttp
 
-print("LOGIN RESPONSE: ", login())
+USERS = ["UpalaZuba1", "UpalaZuba2", "UpalaZuba3", "UpalaZuba4"]
+tokens = []
+
+for user in USERS:
+    tokens.append(login(user))
+
+createGame("UpalaZuba2", "UpalaZuba3", "UpalaZuba4")
+
+async def joinGameMain():
+
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for token in tokens:
+            tasks.append(joinGame(session, token))
+        responses = await asyncio.gather(*tasks)
+
+    for index, response in enumerate(responses, 1):
+        print(f"Response {index}: {response}")
+
+asyncio.run(joinGameMain())
+
