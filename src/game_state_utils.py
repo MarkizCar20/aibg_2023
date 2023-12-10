f17f314607c9e66a9ba41cc3a0ac7d19cb71a9ec
Bapponi <aleksandarbubalo99@gmail.com>
2023-12-10 05:50:18 +0100

    upotpunjeno dohvatanje stvari, mozda je jos ostalo da se dohvati najblize polje odredjenog tipa

--- src/game_state_utils.py
+++ src/game_state_utils.py
@@ -5,13 +5,11 @@ myID = 0
 def getMyID():
     return state["playerIdx"]
 
-
 def findPlayerByID(id):
     if state["gameState"][f"player{id}"] != {}:
         return state["gameState"]["player1"]
     else:
-        return -1
-    
+        return -1    
 
 def getHealth(id):
     player_available = findPlayerByID(id)
@@ -20,45 +18,78 @@ def getHealth(id):
     else:
         return state["gameState"][f"player{id}"]["health"]
 
-
 def getMyHealth():
     return state["gameState"][f"player{myID}"]["health"]
-
 
 def getAttackPow(id):
     player_available = findPlayerByID(id)
     if player_available == -1:
         return -1
     else:
-        return state["gameState"][f"player{id}"]["attackPower"]
-    
+        return state["gameState"][f"player{id}"]["attackPower"]   
 
 def getMyAttackPow():
     return state["gameState"][f"player{myID}"]["attackPower"]
-
 
 def getQ(id):
     player_available = findPlayerByID(id)
     if player_available == -1:
         return -1
     else:
-        return state["gameState"][f"player{id}"]["q"]
-    
+        return state["gameState"][f"player{id}"]["q"]   
 
 def getR(id):
     player_available = findPlayerByID(id)
     if player_available == -1:
         return -1
     else:
-        return state["gameState"][f"player{id}"]["r"]
-    
+        return state["gameState"][f"player{id}"]["r"] 
 
 def getMyQ():
     return state["gameState"][f"player{myID}"]["q"]
 
-
 def getMyR():
     return state["gameState"][f"player{myID}"]["r"]
+
+def getScore(id):
+    player_available = findPlayerByID(id)
+    if player_available == -1:
+        return -1
+    else:
+        return state["gameState"][f"player{id}"]["score"]
+
+def getMyScore():
+    return state["gameState"][f"player{myID}"]["score"]
+
+def getSkull(id):
+    player_available = findPlayerByID(id)
+    if player_available == -1:
+        return -1
+    else:
+        return state["gameState"][f"player{id}"]["skull"]
+    
+def getMySkull():
+    return state["gameState"][f"player{myID}"]["skull"]
+    
+def getScoreLvl(id):
+    player_available = findPlayerByID(id)
+    if player_available == -1:
+        return -1
+    else:
+        return state["gameState"][f"player{id}"]["scoreLevel"]
+
+def getMyScoreLvl():
+    return state["gameState"][f"player{myID}"]["scoreLevel"]
+
+def getSword(id):
+    player_available = findPlayerByID(id)
+    if player_available == -1:
+        return -1
+    else:
+        return state["gameState"][f"player{id}"]["sword"]
+
+def getMySword():
+    return state["gameState"][f"player{myID}"]["sword"]
 
 def getDistance(id1, id2):
     player_available1 = findPlayerByID(id1)
@@ -68,13 +99,24 @@ def getDistance(id1, id2):
         return int((abs(getQ(id1)-getQ(id2))+abs(getR(id1)-getR(id2)))/2)
     else:
         return -1
+    
+def getTile(q, r):
+    try:
+        tiles = state["gameState"]["map"]["tiles"]
 
-#treba dohvatiti q i r od kovcega
-#treba dohvatiti ostatak stvari kod player-a
-#treba dohvatiti polje sa odredjenim koordinatama
+        # Iterate through the tiles to find the one with matching q and r
+        for row in tiles:
+            for tile in row:
+                if tile["q"] == q and tile["r"] == r:
+                    return tile
+
+        # If coordinates not found, raise an exception
+        raise ValueError("Coordinates not found in the map.")
+
+    except KeyError:
+        raise ValueError("Invalid game state structure.")
+
 #treba dohvatiti najblize lisce ili drvo
-
-# if q and r in state["gameState"]["map"]["tiles"]
 
 # testiranje
 
@@ -92,3 +134,5 @@ print(getMyR())
 
 print(getDistance(1, 4))
 print(getDistance(1, 3))
+
+print(getTile(1, -13))
